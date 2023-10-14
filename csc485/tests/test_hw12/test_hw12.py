2ee47c54a3cf9352c5ef07956bffda31bb08f1a2
John-Dublanyk <jdubl001@plattsburgh.edu>
2023-10-14 17:06:20 -0400

    hw12 updated

--- csc485/tests/test_hw12/test_hw12.py
+++ csc485/tests/test_hw12/test_hw12.py
@@ -1,16 +1,29 @@
 import pytest
 from projects.hw12.hw12 import compute_complexity
 
-@pytest.mark.parametrize('password, expected_complexity', [
-    ('johndub123', 0.0),
-    ('j0hn@ub1!', 14.285714285714286),
-    ('!^^&', 100.0),
-    ('', 0.0),
+#testing johndub123 as a password with no complex characters
+#testing john@ub1^ with mutilpe complex characters
+#test ^^^@ with all complex characters
+@pytest.mark.parametrize('password', [
+    'johndub123',
+    'john@ub1^',
+    '^^^@',
+
 ])
-def test_compute_complexity(password, expected_complexity):
+def test_compute_complexity(password):
     """
-    Test the complexity computation for the password cases.
+    Testing the complexity for the different password cases, we are looking how complex each possible password can be.
     """
+    expected_complexity = {
+        'johndub123': 0.0,
+        'john@ub1^': 22.22222222222222,
+        '^^^@': 100.0,
+
+    }
+
     result = compute_complexity(password)
-    assert result == expected_complexity, f"Expected complexity: {expected_complexity}, but got {result} for password: '{password}'"
+    assert result == expected_complexity[password], f"Expected complexity: {expected_complexity[password]}, but got {result} for password: '{password}'"
+
+
+
 
--- csc485/tests/test_hw12/test_hw12b.py
+++ csc485/tests/test_hw12/test_hw12b.py
@@ -1,14 +1,17 @@
 import pytest
 from projects.hw11.hw11 import get_formal_name
 
+"""Testing the fruit name with a fruit's formal name.
+ If the fruit matches it's formal name the test will pass. 
+ we are testing certain fruits below with formal name."""
 
-@pytest.mark.parametrize('fruit', 'expected_name', [
+@pytest.mark.parametrize('expected_name', [
     ('apple', 'Malus domestica'),
     ('pear', 'Pyrus'),
     ('banana', 'Musa acuminata'),
     ('grape', 'Vitis vinifera'),
-
 ])
-def test_get_formal_name(fruit, expected_name):
+def test_get_formal_name(expected_name):
+    fruit, expected_name = expected_name  # Unpack the tuple
     result = get_formal_name(fruit)
     assert result == expected_name, f"Expected '{expected_name}' but got '{result}' for {fruit}"
