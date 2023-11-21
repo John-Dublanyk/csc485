7e91f6a8dd0bb65d198b71869a384b0010621363
John-Dublanyk <jdubl001@plattsburgh.edu>
2023-11-21 09:08:57 -0500

    HW18 update, push.yml, setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='csc485',
+   version='1.0',
+   description='code work for the class CSC 485',
+   author='John Dublanyk',
+   packages=['csc485'],
+)
